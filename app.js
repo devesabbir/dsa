562eb397e9a78813e287f65c2df26a096248d60a
devesabbir <devssabbir@gmail.com>
2024-02-03 23:18:02 +0600

    Practice DSA

---
+++ app.js
@@ -0,0 +1,38 @@
+// DSA
+// const my_array = [7, 12, 9, 4, 11];
+// let min_value = my_array[0];
+
+// my_array.forEach((i) => {
+//   if (i < min_value) {
+//     min_value = i;
+//   }
+// });
+
+// The Fibonacci Number Algorithm
+// 1. Implementation Using a For Loop
+// let num1 = 0;
+// let num2 = 1;
+
+// for (let i = 0; i < 18; i++) {
+//   let newNumber = num1 + num2;
+//   console.log(newNumber); // ibonacci numbers
+//   num1 = num2;
+//   num2 = newNumber;
+// }
+
+// The Fibonacci Number Algorithm
+// 2. Implementation Using Recursion
+// let count = 1;
+
+// function fibonacci(num1, num2) {
+//   if (count < 19) {
+//     let newNumber = num1 + num2;
+//     console.log(newNumber);
+//     num1 = num2;
+//     num2 = newNumber;
+//     count++;
+//     fibonacci(num1, num2);
+//   }
+// }
+
+// fibonacci(0, 1);
